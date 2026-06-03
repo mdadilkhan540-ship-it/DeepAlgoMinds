c971544b20b3a3415298cfb3404274ced8ad8ee6
mdadilkhan540-ship-it <mdadilkhan540@gmail.com>
2026-06-03 08:28:31 +0530

    Add files via upload
    Uploaded Python solutions for:
    - Maximum Subarray
    - Rearrange Array Elements by Sign
    - Rotate Array
    - Sort Colors
    -Intersection of two arrays
    - kth largest elements in array
    
    Practiced array manipulation and algorithmic problem solving.

---
+++ deep_algo_minds/Rearrange array elements by sign.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rearrangeArray(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        res = [0] * n
+
+        pos, neg = 0, 1
+
+        for num in nums:
+            if num > 0:
+                res[pos] = num
+                pos += 2
+            else:
+                res[neg] = num
+                neg += 2
+
+        return res
